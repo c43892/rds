7e794637b5f57f39d3f95f97ff0ae81175058cd6
rainland <lyz901222@qq.com>
2018-07-08 20:29:10 +0800

    生成世界地图

--- common/logic/WorldMapGenerator.ts
+++ common/logic/WorldMapGenerator.ts
@@ -7,13 +7,16 @@ class WorldMapGenerator{
 
         var nodes = WorldMapGenerator.createNodes(w, cfg);
 
-        WorldMapGenerator.createRoutes(cfg, nodes, rand);
+        nodes = WorldMapGenerator.createRoutes(cfg, nodes, rand);
+
+        nodes = WorldMapGenerator.deleteRepeatRoutes(nodes);//去掉重复路线
 
         return w;
     }
 
-    public static createNodes(worldMap, cfg):WorldMapNode[][]{//根据地图大小生成所有节点
-        var nodes: WorldMapNode[][];
+    //根据地图大小生成所有节点
+    public static createNodes(worldMap, cfg):WorldMapNode[][]{
+        var nodes = new Array();
         var height = cfg.totalLevels;
         var width = cfg.width;
 
@@ -22,8 +25,9 @@ class WorldMapGenerator{
             for(var x = 0; x < width; x++){
                 row.push(new WorldMapNode(x, y));
             }
-            nodes.push(row);
+            nodes[y] = row;
         }
+        Utils.log("createNodes runned");
         return nodes;
     }
 
@@ -32,35 +36,147 @@ class WorldMapGenerator{
         var rowSize = cfg.width;
 
         for(var i = 0; i < cfg.routesMax; i++){
-            var startNode = rd.nextInt(0, rowSize-1);
+            var startNode = rd.nextInt(0, rowSize);
 
             if(i == 0)
             var firstStartRow = startNode;//记下第一次的随机结果
             while(i == 1 && startNode == firstStartRow){//重复随机,保证至少有两条不同的出发路线
-                startNode = rd.nextInt(0, rowSize-1);
+                startNode = rd.nextInt(0, rowSize);
             }
-            WorldMapGenerator.createNextRoutes(nodes, new WorldMapRoute(new WorldMapNode(startNode, -1),new WorldMapNode(startNode, 0)) , rd);//继续生成路线
+
+            var startRoute = new WorldMapRoute(WorldMapNode.getNode(startNode, -1, nodes), WorldMapNode.getNode(startNode, 0, nodes));
+            WorldMapGenerator.createNextRoutes(nodes, startRoute, rd);//继续生成路线
+
+
         }
+        Utils.log("createRoutes runned");
+        return nodes;
     }
 
     public static createNextRoutes(nodes:WorldMapNode[][], route:WorldMapRoute, rd:SRandom){
-        var currentRow = route.dstNode.y;//当前层
+        var currentNode = route.dstNode;//当前点
+        var currentRow = currentNode.y;//当前层
         var currentRowSize = nodes[currentRow].length;//当前层节点数
-        var xOffset;
-        
-        if(route.dstNode.x == 0)
-            xOffset = rd.nextInt(0, 1);
-        else if(route.dstNode.x == currentRowSize - 1)
-            xOffset = rd.nextInt(-1, 0);
-        else
-            xOffset = rd.nextInt(-1, 1);
-        
-        var targetNode = new WorldMapNode(route.dstNode.x + xOffset, route.dstNode.y + 1);//目标点
 
+        if(currentRow == nodes.length - 2){//判断是否是通往BOSS点的路线
+            var centerX = (currentRowSize + 1) / 2;
+            targetNode = WorldMapNode.getNode(centerX, currentRow + 1, nodes);
+            var newRoute = new WorldMapRoute(currentNode, targetNode);
+            currentNode.addRoute(newRoute);
+            
+            Utils.log("createNextRoutes runned");
+            return nodes;
+        }
+        
+        //根据节点在本层的位置确定目标节点的x方向偏移量
+        var xOffset;
+        var min, max;
+        if(route.dstNode.x == 0){
+            min = 0;
+            max = 1;
+        }
+        else if(route.dstNode.x == currentRowSize - 1){
+            min = -1;
+            max = 0;
+        }
+        else{
+            min = -1;
+            max = 1;
+        }
+        xOffset = rd.nextInt(min, max + 1);
+        var newNodeX = route.dstNode.x + xOffset;
+        var newNodeY = route.dstNode.y + 1;
+        var targetNode = WorldMapNode.getNode(newNodeX, newNodeY + 1, nodes);//选定目标点
+
+
+        //根据节点的共同祖先优化路线
         var minAncestorGap = 3;
         var maxAncestorGap = 5;
+        var parents = targetNode.getParents();
+        if(parents.length != 0){
+            for(var parent of parents){
+                if(parent != currentNode){
+                    var ancestor = WorldMapNode.getCommonAncestor(parent ,currentNode, maxAncestorGap);
+                    if(ancestor != null){
+                        var ancestorGap = newNodeY - ancestor.y;
+                        if(ancestorGap < minAncestorGap){
+                            if(targetNode.x > currentNode.x){
+                                newNodeX = currentNode.x + rd.nextInt(-1, 1);
+                                if(newNodeX < 0)
+                                    newNodeX = 0;
+                            }
+                            else if(targetNode.x == currentNode.x){
+                                newNodeX = currentNode.x + rd.nextInt(-1, 2);
+                                if(newNodeX < 0)
+                                    newNodeX = currentNode.x + 1;
+                                else if(newNodeX > nodes[currentRow + 1].length)
+                                    newNodeX = currentNode.x - 1;
+                            }
+                            else{
+                                newNodeX = currentNode.x + rd.nextInt(0, 2);
+                                if(newNodeX > nodes[currentRow + 1].length)
+                                    newNodeX = currentNode.x;
+                            }
+                            targetNode = WorldMapNode.getNode(newNodeX, newNodeY + 1, nodes);
+                        }
+                    }
+                }
+            }
+        }
 
-        var currentNodeParents = route.dstNode.getParents;
-        
+        //保证路线不交叉
+        if(currentNode.x != 0){
+            var leftNode = WorldMapNode.getNode(currentNode.x - 1, currentNode.y, nodes);
+            if(leftNode.hasRoute()){
+                if(leftNode.rightRoute().dstNode.x > newNodeX){
+                    newNodeX = newNodeX + 1;
+                }
+            }
+        }
+        if(currentNode.x < currentRowSize - 1){
+            var rightNote = WorldMapNode.getNode(currentNode.x + 1, currentNode.y, nodes);
+            if(rightNote.hasRoute()){
+                if(rightNote.leftRoute().dstNode.x < newNodeX){
+                    newNodeX = newNodeX - 1;
+                }
+            }            
+        }
+
+        //确定路线的目标节点
+        targetNode = WorldMapNode.getNode(newNodeX, newNodeY, nodes);
+
+        //获得新路线并添加相应点的属性
+        var newRoute = new WorldMapRoute(currentNode, targetNode);
+        currentNode.addRoute(newRoute);
+        targetNode.addParente(currentNode);
+
+        Utils.log("createNextRoutes runned");
+        //继续生成路线
+        return WorldMapGenerator.createNextRoutes(nodes, newRoute, rd);
     }
+
+    //删除多余路线
+    public static deleteRepeatRoutes(nodes:WorldMapNode[][]):WorldMapNode[][]{
+        for(var row of nodes){
+            for(var node of row){
+                if(node.hasRoute()){
+                    var preRoutes:WorldMapRoute[] = [];
+                    var i = 0;
+                    for(var route of node.routes){
+                        for(var testRoute of preRoutes){
+                            if(route.dstNode == testRoute.dstNode)
+                                i++;
+                            
+                        }
+                        if(i == 0)
+                        preRoutes.push(route);
+                    }
+                    node.routes = preRoutes;
+                }
+            }
+        }
+        Utils.log("deleteRepeatRoutes runned");
+        return nodes;
+    }
+
 }
--- common/logic/WorldMapNode.ts
+++ common/logic/WorldMapNode.ts
@@ -2,24 +2,52 @@
 class WorldMapNode{
     public x:number;//世界地图横坐标
     public y:number;//世界地图层数
-    public routes:WorldMapRoute[];//该点通往上一层的路线
+    public routes:WorldMapRoute[] = [];//该点通往上一层的路线
     public roomType:string;//该点房间类型
-    public leftRoute = this.routes.sort(WorldMapNode.getLeftRoute)[0];//由该点出发最左边的路线
-    public rightRoute = this.routes.sort(WorldMapNode.getRightRoute)[0];//由该点出发最右边的路线
-    public parents:WorldMapRoute[];//由下层通往该点的其他点
+    public parents:WorldMapNode[] = [];//该点的父节点
 
     constructor(x:number, y:number){
         this.x = x;
         this.y = y;
     }
 
-    public getParents(routes:WorldMapRoute[]):WorldMapNode[]{
-        var rs:WorldMapNode[];
-        for(var r of routes){
-            if(r.dstNode == this)
-            rs.push(r.strNode);
-        }
-        return rs;
+
+    public static getNode(x, y, nodes):WorldMapNode{
+        var elem;
+        Utils.NDimentionArrayForeach(nodes, (e) => {
+            if (e.x == x && e.y == y) {
+                elem = e;
+                return true;
+            }
+        });
+        return elem;
+    }
+
+    public hasRoute():boolean{//是否有路线通往下层
+        if(this.routes.length == 0)
+            return false;        
+        else 
+            return true;
+    }
+
+    public getParents(){//获取该点的父节点
+        return this.parents;
+    }
+
+    public addParente(parentNode:WorldMapNode){//添加父节点
+        this.parents.push(parentNode);
+    }
+
+    public addRoute(route:WorldMapRoute){//添加路线
+        this.routes.push(route);
+    }
+
+    public rightRoute():WorldMapRoute{//由该点出发最右边的路线
+        return this.routes.sort(WorldMapNode.getRightRoute)[0];
+    }
+
+    public leftRoute():WorldMapRoute{//由该点出发最左边的路线
+        return this.routes.sort(WorldMapNode.getLeftRoute)[0];
     }
 
     public static getLeftRoute = function (r1, r2){//找该点最左边路线的规则
@@ -44,6 +72,66 @@ class WorldMapNode{
             return 1;
         }
         else return 0;
+    }
+
+    public static getLeftNode = function (n1, n2){//找该点最左边节点的规则
+        var x1 = n1.x;
+        var x2 = n2.x;
+        if(x1 < x2){
+            return 1;
+        }
+        else if(x1 > x2){
+            return -1;
+        }
+        else return 0;
+    }
+
+    public static getRightNode = function (n1, n2){//找该点最左边节点的规则
+        var x1 = n1.x;
+        var x2 = n2.x;
+        if(x1 < x2){
+            return -1;
+        }
+        else if(x1 > x2){
+            return 1;
+        }
+        else return 0;
+    }
+
+    public static getCommonAncestor(node1:WorldMapNode ,node2:WorldMapNode ,depth){//找寻同层两点的共同祖先点
+        var rightNode:WorldMapNode;
+        var leftNode:WorldMapNode;
+    
+        if (node1.x < node2.y){
+            var leftNode = node1;
+            rightNode = node2;
+        } else{
+            leftNode = node2;
+            rightNode = node1;
+        }
+        var currentY = node1.y;
+
+        while ((currentY >= 0) && (currentY >= node1.y - depth)){
+            if(leftNode.getParents().length == 0 || rightNode.getParents().length == 0)
+                return null;
+            
+
+            leftNode = WorldMapNode.getMaxXNode(leftNode.getParents());
+            rightNode = WorldMapNode.getMinXNode(rightNode.getParents());
+            if(leftNode == rightNode)
+                return leftNode;
+            
+            currentY--;
+        }
+     return null;
+    }
+
+    private static getMaxXNode(nodes:WorldMapNode[]):WorldMapNode{
+        return nodes.sort(WorldMapNode.getLeftNode)[0];
+    }
+
+    private static getMinXNode(nodes:WorldMapNode[]):WorldMapNode{
+        return nodes.sort(WorldMapNode.getRightNode)[0];
     }
 
 }
