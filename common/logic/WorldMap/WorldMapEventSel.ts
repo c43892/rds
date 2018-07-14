ca22f32448e43d3f64996ebdc0f2e25539d1d591
chenm <1460262228@qq.com>
2018-07-14 16:22:53 +0800

    增加连续选项的死亡检查

--- common/logic/WorldMap/WorldMapEventSel.ts
+++ common/logic/WorldMap/WorldMapEventSel.ts
@@ -149,7 +149,10 @@ class WorldMapEventSelFactory {
             }
         }, sel),
         "redirectSelGroup": (sel:WMES, p:Player, ps) => this.exec(async () => {
-            await this.openEventSels(p, ps.group);
+             if (p.isDead())
+                sel.exit = () => true;
+            else
+                await this.openEventSels(p, ps.group);
         }, sel),
         "sequence": (sel:WMES, p:Player, ps) => {
             var subSels = [];
@@ -179,7 +182,7 @@ class WorldMapEventSelFactory {
                 sel.valid = ss.valid;
                 sel.exec = async () => {
                     await ss.exec();
-                    if (n < subSels.length) {
+                    if (n < subSels.length && !p.isDead()) {
                         sel["move2NextSubSel"](n);
                         sel.exit = () => false;
                     } else
