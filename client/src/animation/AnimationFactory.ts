873eee35d6e0a6a6b03e994443414c1d896d9413
chenm <1460262228@qq.com>
2018-08-01 17:03:32 +0800

    增加组合动画机制

--- client/src/animation/AnimationFactory.ts
+++ client/src/animation/AnimationFactory.ts
@@ -3,46 +3,77 @@ class AnimationFactory {
 
     public notifyAniStarted;
 
+    public createAniByCfg(cfg):Promise<void> {
+        if (cfg.type == "aniSeq" || cfg.type == "aniGroup") {
+            var aniArr = [];
+            for (var subCfg of cfg.ps) {
+                var subAni = this.createAniByCfg(subCfg);
+                aniArr.push(subAni);
+            }
+            return this.createAni(cfg.type, aniArr);
+        } else
+            return this.createAni(cfg.type, cfg.ps);
+    }
+
     // 创建指定类型的动画
     public createAni(aniType:string, ps = undefined):Promise<void> {
+        var aw;
         if (aniType == "aniSeq")
-            return this.aniSeq(ps);
+            aw = this.aniSeq(ps);
         else if (aniType == "aniGroup")
-            return this.aniGroup(ps);
+            aw = this.aniGroup(ps);
+        else {
+            var ani:egret.Tween;
+            switch (aniType) {
+                case "elemChanged": ani = this.monsterChanged(ps.m); break;
+                case "playerChanged": ani = this.playerChanged(); break;
+                case "playerAttackMonster": ani = this.playerAttackMonster(); break;
+                case "monsterAttackPlayer": ani = this.monsterAttackPlayer(); break;
+                case "moving": ani = this.moving(ps.obj, ps.path); break;
+                case "fade": ani = this.fade(ps.img, ps); break;
+                case "moneyMoving": ani = undefined; break;
+                case "suckBlood": ani = undefined; break;
+                case "monsterTakeElem": ani = undefined; break;
+                case "cycleMask": ani = this.cycleMask(ps.img, ps); break;
+            }
 
-        var ani:egret.Tween;
-        switch (aniType) {
-            case "elemChanged": ani = this.monsterChanged(ps.m); break;
-            case "playerChanged": ani = this.playerChanged(); break;
-            case "playerAttackMonster": ani = this.playerAttackMonster(); break;
-            case "monsterAttackPlayer": ani = this.monsterAttackPlayer(); break;
-            case "moving": ani = this.moving(ps.obj, ps.path); break;
-            case "fade": ani = this.fade(ps.img, ps); break;
-            case "moneyMoving": ani = undefined; break;
-            case "suckBlood": ani = undefined; break;
-            case "monsterTakeElem": ani = undefined; break;
-            case "cycleMask": ani = this.cycleMask(ps.img, ps); break;
+            if (!ani) Utils.log("unknown aniType: " + aniType);
+            aw = ani ? new Promise<void>((r, _) => ani.call(r)) : Utils.delay(10);
         }
-
-        if (!ani) Utils.log("unknown aniType: " + aniType);
         
-        var aw = ani ? new Promise<void>((r, _) => ani.call(r)) : Utils.delay(10);
+        aw["name"] = (ps && ps.name) ? ps.name : undefined;
         aw["ani"] = ani;
+        aw["onStarted"] = [
+            // () => Utils.log("ani: " + aw["name"] + " started"),
+        ];
+        aw["onEnded"] = [
+            // () => Utils.log("ani: " + aw["name"] + " ended"),
+        ];
 
         var notifyStart = ps && !ps.noWait;
+        aw["start"] = () => {
+            for (var cb of aw["onStarted"]) cb();
+            if (ani) ani.setPaused(false);
+            else if (aw["startimpl"]) aw["startimpl"]();
+            if (notifyStart) this.notifyAniStarted(aw, aniType, ps);
+        };
+
+        aw["pause"] = () => {
+            // Utils.log("ani: " + aw["name"] + " paused");
+            if (ani) ani.setPaused(true);
+            else if (aw["pauseimpl"]) aw["pauseimpl"]();
+        };
 
         // 不要自动播放
-        if (ani && ps.manuallyStart) {
-            ani.setPaused(true);
-            aw["start"] = () => {
-                ani.setPaused(false);
-                if (notifyStart)
-                    this.notifyAniStarted(aw, aniType, ps);
-            }
-        } else { // 自动播放
-            if (notifyStart)
-                this.notifyAniStarted(aw, aniType, ps);
-        }
+        if (ani && ps.manuallyStart)
+            aw["pause"]();
+        else // 自动播放
+            aw["start"]();
+        
+        aw.then(() => {
+            for (var cb of aw["onEnded"])
+                cb()
+        });
 
         return aw;
     }
@@ -86,23 +117,21 @@ class AnimationFactory {
     // 渐隐渐显
     fade(g:egret.DisplayObject, ps):egret.Tween {
         // properties from
-        g.alpha = ps.fa != undefined ? ps.fa : g.alpha;
-        g.x = ps.fx != undefined ? ps.fx : g.x;
-        g.y = ps.fy != undefined ? ps.fy : g.y;
-        g.width = ps.fw != undefined ? ps.fw : g.width;
-        g.height = ps.fh != undefined ? ps.fh : g.height;
+        var fromAttrs = {};
+        if (ps.fx != undefined) fromAttrs["x"] = ps.fx;
+        if (ps.fy != undefined) fromAttrs["y"] = ps.fy;
+        if (ps.fw != undefined) fromAttrs["width"] = ps.fw;
+        if (ps.fh != undefined) fromAttrs["height"] = ps.fh;
+        if (ps.fa != undefined) fromAttrs["alpha"] = ps.fa;
 
-        // properties to
-        var x = ps.tx != undefined ? ps.tx : g.x;
-        var y = ps.ty != undefined ? ps.ty : g.y;
-        var w = ps.tw != undefined ? ps.tw : g.width;
-        var h = ps.th != undefined ? ps.th : g.height;
-        var a = ps.ta != undefined ? ps.ta : g.alpha;
-        var time = ps.time;
+        var toAttrs = {};
+        if (ps.tx != undefined) toAttrs["x"] = ps.tx;
+        if (ps.ty != undefined) toAttrs["y"] = ps.ty;
+        if (ps.tw != undefined) toAttrs["width"] = ps.tw;
+        if (ps.th != undefined) toAttrs["height"] = ps.th;
+        if (ps.ta != undefined) toAttrs["alpha"] = ps.ta;
 
-        var tw = egret.Tween.get(g);
-        tw.to({x:x, y:y, width:w, height:h, alpha:a}, time, ps.mode);
-        return tw;
+        return egret.Tween.get(g).to(fromAttrs, 0).to(toAttrs, ps.time, ps.mode);
     }
 
     // 环形转圈
@@ -138,42 +167,47 @@ class AnimationFactory {
 
     // 动画序列
     aniSeq(subAnis:Promise<void>[]):Promise<void> {
-        var aniArr = [];
-        for (var subAni of subAnis) {
-            var ani = subAni["ani"];
-            ani.setPaused(true);
-            aniArr.push(ani);
+        var aw;
+        var curAni;
+        for (var i = 0; i < subAnis.length; i++) {
+            subAnis[i]["pause"]();
+            (() => subAnis[i]["onStarted"].push(() => curAni = subAnis[i]))();
+
+            if (i < subAnis.length - 1)
+                (() => subAnis[i]["onEnded"].push(subAnis[i + 1]["start"]))();
+            else
+                aw = new Promise<void>((r, _) => subAnis[subAnis.length - 1]["onEnded"].push(r));
         }
 
-        for (var i = 0; i < aniArr.length - 1; i++) {            
-            var curAni = aniArr[i];
-            var nextAni = aniArr[i + 1];
-            var nextSubAni = subAnis[i + 1];
-            var moveOn = () => {
-                Utils.log("move on ani: " + i);
-                nextSubAni["start"]();
-            };
-            curAni.call(moveOn);
-        }
-
-        aniArr[0].setPaused(false);
-        var aw = new Promise<void>((r, _) => aniArr[aniArr.length - 1].call(r));
+        aw["startimpl"] = () => subAnis[0]["start"]();
+        aw["pauseimpl"] = () => curAni["pause"]();
 
         return aw;
     }
 
     // 同时播放的动画组合
     aniGroup(subAnis:Promise<void>[]):Promise<void> {
-        var aniArr = [];
-        for (var subAni of subAnis) {
-            var ani = subAni["ani"];
-            ani.setPaused(true);
-            aniArr.push(ani);
-        }
+        var aw = new Promise<void>((r, _) => {
+            var endCnt = subAnis.length;
+            for (var ani of subAnis) {
+                ani["pause"]();
+                (() => ani["onEnded"].push(() => {
+                    endCnt--;
+                    if (endCnt <= 0)
+                        r();
+                }))();
+            }
+        });
 
-        // todo
+        aw["startimpl"] = () => {
+            for (var ani of subAnis)
+                ani["start"]();
+        };
 
-        var aw = new Promise<void>((r, _) => aniArr[aniArr.length - 1].call(r));
+        aw["pauseimpl"] = () => {
+            for (var ani of subAnis)
+                ani["pause"]();
+        };
 
         return aw;
     }
