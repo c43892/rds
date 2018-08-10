ea827829e229eb571f83cbf95f96de5708c298f1
chenm <1460262228@qq.com>
2018-08-10 22:30:41 +0800

    修改版本号

--- common/logic/Version.ts
+++ common/logic/Version.ts
@@ -1,6 +1,6 @@
 class Version {
     // 当前版本号
-    public static currentVersion:number = 0.11;
+    public static currentVersion:number = 0.12;
 
     // 版本是否兼容
     public static isCompatible(oldVer:number) {
