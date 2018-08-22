8f1d1855975749179c73b8d446226a0caecb8c1a
chenm <1460262228@qq.com>
2018-08-22 20:38:18 +0800

    增加 refElems 的配置

--- client/src/view/MainView.ts
+++ client/src/view/MainView.ts
@@ -391,7 +391,11 @@ class MainView extends egret.DisplayObjectContainer {
             
             var res = attrs.elemImg ? attrs.elemImg : type;
             if (!attrs.invisible) {
-                if (attrs.repRes) {
+                if (attrs.refElems) {
+                    for (var re of attrs.refElems)
+                        es.push(re);
+                }
+                else if (attrs.repRes) {
                     for (var r of attrs.repRes)
                         resArr.push(r + "_png");
                 } else
