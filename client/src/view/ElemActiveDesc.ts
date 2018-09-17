22ea6ee251a0959544dfd3551aaab36bb26c54b9
rainland <lyz901222@qq.com>
2018-09-17 21:00:39 +0800

    添加哥布林偷钱数量的动态描述

--- client/src/view/ElemActiveDesc.ts
+++ client/src/view/ElemActiveDesc.ts
@@ -17,6 +17,9 @@ class ElemActiveDesc {
         },
         "BombAbomination" : {
             "rounds": (p:Player, e:Elem) => e["attackInterval"] ? e["attackInterval"] : e.attrs.selfExplode.cnt
+        },
+        "GoblinThief" : {
+            "percent": (p:Player, e:Elem) => Math.floor(p.money * e.attrs.steal.percent / 100) - e.attrs.steal.num
         }
     }
     
