33adabbb9aaf1fb1d1179dfcf8f0dc0c1b7796f2
rainland <lyz901222@qq.com>
2018-09-23 22:12:40 +0800

    修改分配钥匙的关卡逻辑

--- common/logic/LevelLogic/LevelLogicTakeKey.ts
+++ common/logic/LevelLogic/LevelLogicTakeKey.ts
@@ -9,56 +9,52 @@ class LevelLogicTakeKey extends LevelLogic{
             var index = bt.btType.indexOf("_");
             index = index < 0 ? bt.btType.length : index;
             var btType = bt.btType.substring(0 , index);
-
-            switch(btType){
-                case "normal":{
-                    var takeNum = bt.srand.nextInt(keys.length - 1, keys.length + 1);
-                    await this.normalMonsterTakeKey(bt, keys, takeNum);
-                    break;
-                }
-                case "senior":{
-                    var elites = <Monster[]>bt.level.map.findAllElems((e:Elem) => e instanceof Monster && e.isBig());
-                    if(elites.length > 1){
-                        for(var elite of elites){
-                            for(var i = 0; i < 2; i++){
-                                var key = bt.level.map.findFirstElem((e:Elem) => e.type == "Key");
-                                await bt.implMonsterTakeElems(elite, [key], true);
-                                keys = Utils.remove(keys, key);
-                            }
-                        }
-                    }
-                    else if(elites.length == 1) {
-                        var elite = elites[0];
-                        for(var i = 0; i < 4; i++){
+            var seniorTypes = ["senior", "randomEgg"];
+            var bossTypes = ["boss", "slimeKing"];
+            
+            if(btType == "normal"){
+                var takeNum = bt.srand.nextInt(keys.length - 1, keys.length + 1);
+                await this.normalMonsterTakeKey(bt, keys, takeNum);
+            }
+            else if(Utils.indexOf(seniorTypes, (t:string) => t == btType) > -1){
+                var elites = <Monster[]>bt.level.map.findAllElems((e:Elem) => e instanceof Monster && e.isBig());
+                if(elites.length > 1){
+                    for(var elite of elites){
+                        for(var i = 0; i < 2; i++){
                             var key = bt.level.map.findFirstElem((e:Elem) => e.type == "Key");
                             await bt.implMonsterTakeElems(elite, [key], true);
                             keys = Utils.remove(keys, key);
                         }
                     }
-                    if(keys.length > 0){
-                        var takeByNormal = bt.srand.nextInt(keys.length - 1, keys.length + 1);
-                        await this.normalMonsterTakeKey(bt, keys, takeByNormal);
+                }
+                else if(elites.length == 1) {
+                    var elite = elites[0];
+                    for(var i = 0; i < 4; i++){
+                        var key = bt.level.map.findFirstElem((e:Elem) => e.type == "Key");
+                        await bt.implMonsterTakeElems(elite, [key], true);
+                        keys = Utils.remove(keys, key);
                     }
-                    break;
                 }
-                case "boss":{
-                    var boss = <Monster>bt.level.map.findAllElems((e:Elem) => e instanceof Monster && e.isBoss)[0];
-                    for(var i = 0; i < 4; i++)
-                        await bt.implMonsterTakeElems(boss, [keys[i]], true);
-                    
-                    var leftKeys = [];
-                    for(var j = 0; j < keys.length - 4; j++)
-                        leftKeys.push(keys[4 + j])
+                if(keys.length > 0){
+                    var takeByNormal = bt.srand.nextInt(keys.length - 1, keys.length + 1);
+                    await this.normalMonsterTakeKey(bt, keys, takeByNormal);
+                }
+            }
+            else if(Utils.indexOf(bossTypes, (t:string) => t == btType) > -1){
+                var boss = <Monster>bt.level.map.findAllElems((e:Elem) => e instanceof Monster && e.isBoss)[0];
+                for(var i = 0; i < 4; i++)
+                    await bt.implMonsterTakeElems(boss, [keys[i]], true);
+                
+                var leftKeys = [];
+                for(var j = 0; j < keys.length - 4; j++)
+                    leftKeys.push(keys[4 + j])
 
-                    await this.normalMonsterTakeKey(bt, leftKeys, leftKeys.length);
-                    break;
-                }
-                default:{
-                    var takeNum = bt.srand.nextInt(keys.length - 1, keys.length + 1);
-                    await this.normalMonsterTakeKey(bt, keys, takeNum);
-                    break;
-                }
-            }    
+                await this.normalMonsterTakeKey(bt, leftKeys, leftKeys.length);
+            }
+            else{
+                var takeNum = bt.srand.nextInt(keys.length - 1, keys.length + 1);
+                await this.normalMonsterTakeKey(bt, keys, takeNum);
+            }
         }
     }
 
