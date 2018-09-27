203e1fe7749dbe4385ff129bcfb0613caf26095a
chenm <1460262228@qq.com>
2018-09-27 16:08:54 +0800

    药剂改名

--- common/logic/player/Occupation.ts
+++ common/logic/player/Occupation.ts
@@ -17,7 +17,7 @@ class Occupation {
     // 护士
     static makeNurse(p:Player):Player {
         p.addRelic(<Relic>ElemFactory.create("MedicineBox")); // 初始遗物
-        p.addItem(<Prop>ElemFactory.create("HpCapsule")); // 初始物品
+        p.addItem(<Prop>ElemFactory.create("HpPotion")); // 初始物品
         return p;
     }
 
