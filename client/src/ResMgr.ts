79cac7fee7da05cb988e5ef6d7526b08f6d79aa9
chenm <1460262228@qq.com>
2018-09-27 21:14:44 +0800

    处理位图字体的微信兼容问题

--- client/src/ResMgr.ts
+++ client/src/ResMgr.ts
@@ -1,35 +1,35 @@
 class ResMgr {
 
-    // static resMap = {};
-    // public static getRes(name:string):any {
-    //     return ResMgr.resMap[name];
-    // }
-
-    // public static URLPrefix = "https://rds.wudouwxg.xyz/resource/";
-    // public static async loadResGroup(group, eventHandler) {
-    //     var items = RES.getGroupByName(group);
-    //     var finished = 0;
-    //     return new Promise<void>((r, _) => {
-    //         items.forEach((it, i) => {
-    //             RES.getResByUrl(ResMgr.URLPrefix + it.url, (res) => {                    
-    //                 finished++;
-    //                 ResMgr.resMap[it.name] = res;
-
-    //                 if (eventHandler)
-    //                     eventHandler.onProgress(finished, items.length);
-
-    //                 if (finished == items.length)
-    //                     r();
-    //             }, this, it.type);
-    //         });
-    //     });
-    // }
-
+    static resMap = {};
     public static getRes(name:string):any {
-        return RES.getRes(name);
+        return ResMgr.resMap[name];
     }
 
+    public static URLPrefix = "https://rds.wudouwxg.xyz/resource/";
     public static async loadResGroup(group, eventHandler) {
-        await RES.loadGroup(group, 0, eventHandler);
+        var items = RES.getGroupByName(group);
+        var finished = 0;
+        return new Promise<void>((r, _) => {
+            items.forEach((it, i) => {
+                RES.getResByUrl(ResMgr.URLPrefix + it.url, (res) => {                    
+                    finished++;
+                    ResMgr.resMap[it.name] = res;
+
+                    if (eventHandler)
+                        eventHandler.onProgress(finished, items.length);
+
+                    if (finished == items.length)
+                        r();
+                }, this, it.type);
+            });
+        });
     }
+
+    // public static getRes(name:string):any {
+    //     return RES.getRes(name);
+    // }
+
+    // public static async loadResGroup(group, eventHandler) {
+    //     await RES.loadGroup(group, 0, eventHandler);
+    // }
 }
