fd847f67e88cbf6b43e699862ca6a868f17b17ce
chenm <1460262228@qq.com>
2018-10-07 16:51:18 +0800

    增加突袭效果

--- client/src/view/AniView.ts
+++ client/src/view/AniView.ts
@@ -373,6 +373,7 @@ class AniView extends egret.DisplayObjectContainer {
         img.y = toPos.y + sv.height / 2;
         img.alpha = 1;
         img.scaleX = img.scaleY = 1;
+        AniUtils.tipAt(ViewUtils.getTipText("sneaking"), {x:toPos.x + 50, y:toPos.y + 10}, 40, 0xffffff);
         this.aniFact.createAni("tr", {
             obj:img, time: 2000,
             tsx:5, tsy:5, ta:0, mode:egret.Ease.backOut, noWait:true
