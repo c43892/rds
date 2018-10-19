da563262cfef911db29c5adc95205f5b25c15a39
chenm <1460262228@qq.com>
2018-10-19 19:00:29 +0800

    处理资源合并 bug

--- common/logic/Version.ts
+++ common/logic/Version.ts
@@ -1,6 +1,6 @@
 class Version {
     // 当前版本号
-    public static currentVersion:number = 0.23;
+    public static currentVersion:number = 0.24;
 
     // 版本是否兼容
     public static isCompatible(oldVer:number) {
