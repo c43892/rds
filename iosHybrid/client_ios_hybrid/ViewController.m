c9624a86eca18874048605e6b975c9d811e056a8
IvyLingZhijing <ivyling063@gmail.com>
2019-02-01 22:05:16 -0500

    modify xcode project settings

--- iosHybrid/client_ios_hybrid/ViewController.m
+++ iosHybrid/client_ios_hybrid/ViewController.m
@@ -36,9 +36,9 @@ bool notified;
         [loader start];
     }
     
-    [GADMobileAds configureWithApplicationID:@"ca-app-pub-3940256099942544~3347511713"];
-    [GADRewardBasedVideoAd sharedInstance].delegate = self;
-    [self loadAds];
+    // [GADMobileAds configureWithApplicationID:@"ca-app-pub-3940256099942544~3347511713"];
+    // [GADRewardBasedVideoAd sharedInstance].delegate = self;
+    // [self loadAds];
 }
 
 - (void)didReceiveMemoryWarning {
