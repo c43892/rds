c7fb016f5455151504f1e57d42d859a065225f7b
rainland <lyz901222@qq.com>
2019-05-08 13:43:59 +0800

    版本号更新

--- common/logic/Version.ts
+++ common/logic/Version.ts
@@ -1,6 +1,6 @@
 class Version {
     // 当前版本号
-    public static currentVersion:number = 0.47;
+    public static currentVersion:number = 0.48;
 
     // 版本是否兼容
     public static isCompatible(oldVer:number) {
