e4b76b48370b904ec501cdb30340b34f3af99f74
chenm <1460262228@qq.com>
2019-05-28 21:40:39 -0400

    打包 简体中文 1.15 版本

--- common/logic/Version.ts
+++ common/logic/Version.ts
@@ -1,6 +1,6 @@
 class Version {
     // 当前版本号
-    public static currentVersion:number = 0.48;
+    public static currentVersion:number = 1.15;
 
     // 版本是否兼容
     public static isCompatible(oldVer:number) {
